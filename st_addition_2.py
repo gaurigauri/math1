1fc77f9d57d3a313e919158c5893e61f47b393da
gaurigauri <92418750+gaurigauri@users.noreply.github.com>
2022-06-26 17:34:12 -0700

    Update st_addition_2.py

--- st_addition_2.py
+++ st_addition_2.py
@@ -3,18 +3,15 @@ import streamlit as st
 
 
 def code_to_add_nums():
-    with st.form():
-	num1 = st.number_input("number1", 0, 100,5, 1)
-  	num2 = st.number_input("number2", 0, 100,5, 1)
-      	submitted = st.form_submit_button(label="add them")
-	if submitted:
-		with st.spinner("Adding..."):
-			st.write(f"{num1} + {num2} =  {num1 + num2}\n") 
+    with st.form(key="addition"):
+        num1 = st.number_input("number1", 0, 100, 5, 1)
+        num2 = st.number_input("number2", 0, 100, 4, 1)
+        submitted = st.form_submit_button(label="add them")
+        if submitted:
+            # with st.spinner("Adding..."):
+            st.write(f"{num1} + {num2} =  {num1 + num2}\n")
 
-def main():
-  st.title("Add two numbers")
-  code_to_add_nums()
 
-  
-if __name__ == "__main__ ":
-  main()
+if __name__ == '__main__':
+    st.title("Add two numbers")
+    code_to_add_nums()
