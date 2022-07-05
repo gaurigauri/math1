d1e6d230c707d1b214c3724135eb798c6fa088e3
gaurigauri <92418750+gaurigauri@users.noreply.github.com>
2022-07-04 18:10:12 -0700

    enhance the calculator

--- st_calculator_simple.py
+++ st_calculator_simple.py
@@ -3,9 +3,9 @@ import streamlit as st
 
 def calculator_code():
     with st.form(key="calculator"):
-        num1 = st.number_input("number1", 0, 100000, 1)
-        num2 = st.number_input("number2", 0, 100000, 1)
-        op = st.selectbox("select an option", ["*", "+"])
+        num1 = st.number_input("number1", 1, 100000, 1)
+        num2 = st.number_input("number2", 1, 100000, 1)
+        op = st.selectbox("select an option", ["*", "+", "/", "-"])
         submitted = st.form_submit_button(label="calculate")
         if submitted:
             st.write(f"{num1} {op} {num2} = ")
@@ -13,6 +13,10 @@ def calculator_code():
                 st.write(f"{num1 * num2}")
             elif op == "+":
                 st.write(f"{num1 + num2}")
+            elif op == "/":
+                st.write(f"{num1 / num2}")
+            elif op == "-":
+                st.write(f"{num1 - num2}")
 
 
 if __name__ == '__main__':
